f274af69ecd5c9420aa2763e69eece806b3f84b6
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-01-19 12:32:31 +0100

    feat: order approval use case

--- python/src/domain/order.py
+++ python/src/domain/order.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass, field
 
 from .order_item import OrderItem
 from .order_status import OrderStatus
+from ..use_case.exceptions import ShippedOrdersCannotBeChangedException, ApprovedOrderCannotBeRejectedException, \
+    RejectedOrderCannotBeApprovedException
 
 
 @dataclass
@@ -12,3 +14,24 @@ class Order:
     items: list[OrderItem] = field(default_factory=list)
     tax: float = 0
     status: OrderStatus = OrderStatus.CREATED
+
+    def is_shipped(self):
+        return self.status == OrderStatus.SHIPPED
+
+    def is_rejected(self):
+        return self.status == OrderStatus.REJECTED
+
+    def is_approved(self):
+        return self.status == OrderStatus.APPROVED
+
+    def approve(self, is_request_approved):
+        if self.is_shipped():
+            raise ShippedOrdersCannotBeChangedException()
+
+        if is_request_approved and self.is_rejected():
+            raise RejectedOrderCannotBeApprovedException()
+
+        if not is_request_approved and self.is_approved():
+            raise ApprovedOrderCannotBeRejectedException()
+
+        self.status = OrderStatus.APPROVED if is_request_approved else OrderStatus.REJECTED
--- python/src/use_case/order_approval_use_case.py
+++ python/src/use_case/order_approval_use_case.py
@@ -1,8 +1,5 @@
-from ..domain.order_status import OrderStatus
-from ..repository.order_repository import OrderRepository
-from .exceptions import ShippedOrdersCannotBeChangedException, \
-    RejectedOrderCannotBeApprovedException, ApprovedOrderCannotBeRejectedException
 from .order_approval_request import OrderApprovalRequest
+from ..repository.order_repository import OrderRepository
 
 
 class OrderApprovalUseCase:
@@ -12,14 +9,6 @@ class OrderApprovalUseCase:
     def run(self, request: OrderApprovalRequest) -> None:
         order = self._order_repository.get_by_id(request.order_id)
 
-        if order.status == OrderStatus.SHIPPED:
-            raise ShippedOrdersCannotBeChangedException()
+        order.approve(request.approved)
 
-        if request.approved and order.status == OrderStatus.REJECTED:
-            raise RejectedOrderCannotBeApprovedException()
-
-        if not request.approved and order.status == OrderStatus.APPROVED:
-            raise ApprovedOrderCannotBeRejectedException()
-
-        order.status = OrderStatus.APPROVED if request.approved else OrderStatus.REJECTED
         self._order_repository.save(order)
