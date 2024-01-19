dd9a3894ecd161da2ddc0b57a583278873bf233e
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-01-19 13:58:23 +0100

    feat: applies immutability to order

--- python/src/domain/order.py
+++ python/src/domain/order.py
@@ -8,21 +8,21 @@ from ..use_case.exceptions import ShippedOrdersCannotBeChangedException, Approve
 
 @dataclass
 class Order:
-    id: int = 0
-    total: float = 0
-    currency: str = 'EUR'
-    items: list[OrderItem] = field(default_factory=list)
-    tax: float = 0
-    status: OrderStatus = OrderStatus.CREATED
+    __id: int = 0
+    __total: float = 0
+    __currency: str = 'EUR'
+    __items: list[OrderItem] = field(default_factory=list)
+    __tax: float = 0
+    __status: OrderStatus = OrderStatus.CREATED
 
     def __init__(self, identifier=0, status=OrderStatus.CREATED):
         super().__init__()
-        self.id = identifier
-        self.status = status
-        self.items = []
-        self.currency = "EUR"
-        self.total = 0
-        self.tax = 0
+        self.__id = identifier
+        self.__status = status
+        self.__items = []
+        self.__currency = "EUR"
+        self.__total = 0
+        self.__tax = 0
 
     def is_created(self):
         return self.status == OrderStatus.CREATED
@@ -36,6 +36,18 @@ class Order:
     def is_approved(self):
         return self.status == OrderStatus.APPROVED
 
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def order_items(self):
+        return self.__items
+
+    @property
+    def status(self):
+        return self.__status
+
     def approve(self, is_request_approved):
         if self.is_shipped():
             raise ShippedOrdersCannotBeChangedException()
@@ -46,7 +58,7 @@ class Order:
         if not is_request_approved and self.is_approved():
             raise ApprovedOrderCannotBeRejectedException()
 
-        self.status = OrderStatus.APPROVED if is_request_approved else OrderStatus.REJECTED
+        self.__status = OrderStatus.APPROVED if is_request_approved else OrderStatus.REJECTED
 
     def ship(self):
         if self.is_created() or self.is_rejected():
@@ -55,9 +67,9 @@ class Order:
         if self.is_shipped():
             raise OrderCannotBeShippedTwiceException()
 
-        self.status = OrderStatus.SHIPPED
+        self.__status = OrderStatus.SHIPPED
 
     def add_item(self, item):
-        self.items.append(item)
-        self.total += item.taxed_amount
-        self.tax += item.tax
+        self.__items.append(item)
+        self.__total += item.taxed_amount
+        self.__tax += item.tax
